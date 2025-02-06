e5674dbc222dceaa4f7c7b504d39a9b029015b62
davidsanders1999 <david.sanders@online.de>
2025-02-06 16:22:56 +0100

    Updates

--- config.py
+++ config.py
@@ -1,39 +1,39 @@
-# Allgemeine Annahmen
+# # Allgemeine Annahmen
 
-szenario = 'base_2'
-freq = 5
-wartezeit = 15
-# ladegrenze = 0.8
-netzanschlussfaktor = 0.3
+# szenario = 'base_2'
+# freq = 5
+# wartezeit = 15
+# # ladegrenze = 0.8
+# netzanschlussfaktor = 0.3
 
-# Definition "Vollständige Ladung"
-energie_pro_abschnitt = 80 * 4.5 * 1.26
-sicherheitspuffer = 0.15
+# # Definition "Vollständige Ladung"
+# energie_pro_abschnitt = 80 * 4.5 * 1.26
+# sicherheitspuffer = 0.15
 
 
-# Szenarien
-simulationsfokus = ['Cluster', 'Ladequoten', 'Netzanschluss', 'Leistung', 'Pausenzeiten']
-# fokus = simulationsfokus[1]
+# # Szenarien
+# simulationsfokus = ['Cluster', 'Ladequoten', 'Netzanschluss', 'Leistung', 'Pausenzeiten']
+# # fokus = simulationsfokus[1]
 
-# Clustervariation
-cluster             = [1, 2, 3]
+# # Clustervariation
+# cluster             = [1, 2, 3]
 
-# Ladequotenvariation             
-ladequote_HPC       = [0.4, 0.7, 1] 
-ladequote_MCS       = [0.4, 0.7, 1] 
-ladequote_NCS       = [0.4, 0.7, 1]  
+# # Ladequotenvariation             
+# ladequote_HPC       = [0.4, 0.7, 1] 
+# ladequote_MCS       = [0.4, 0.7, 1] 
+# ladequote_NCS       = [0.4, 0.7, 1]  
 
-# Netzanschlussvariation
-netzanschlussfaktor = [0.1, 0.2, 0.3, 0.4, 1]
+# # Netzanschlussvariation
+# netzanschlussfaktor = [0.1, 0.2, 0.3, 0.4, 1]
 
-# Leistungsvariation
-leistung_hpc        = [0.8, 1, 1.5]
-leistung_mcs        = [0.8, 1, 1.5]
-leistung_ncs        = [0.8, 1, 1.2]
+# # Leistungsvariation
+# leistung_hpc        = [0.8, 1, 1.5]
+# leistung_mcs        = [0.8, 1, 1.5]
+# leistung_ncs        = [0.8, 1, 1.2]
 
-# Pausenzeiten
-pausen_schnell      = [45, 60, 75]
-pausen_nacht    = [540, 600, 660]
+# # Pausenzeiten
+# pausen_schnell      = [45, 60, 75]
+# pausen_nacht    = [540, 600, 660]
 
 
 
@@ -84,15 +84,15 @@ list_szenarien = [
 # 'cl_2_quote_100-100-100_netz_100_pow_100-100-100_pause_45-600_M_28_Pausenzeiten-600',
 # 'cl_2_quote_100-100-100_netz_100_pow_100-100-100_pause_45-660_M_29_Pausenzeiten-660', 
 
-'cl_2_quote_100-100-100_netz_90_pow_100-100-100_pause_45-540_M_30_Netzanschluss-90',
-'cl_2_quote_100-100-100_netz_80_pow_100-100-100_pause_45-540_M_31_Netzanschluss-80', 
-'cl_2_quote_100-100-100_netz_70_pow_100-100-100_pause_45-540_M_32_Netzanschluss-70', # Netzanschluss
-'cl_2_quote_100-100-100_netz_60_pow_100-100-100_pause_45-540_M_33_Netzanschluss-60', 
-'cl_2_quote_100-100-100_netz_50_pow_100-100-100_pause_45-540_M_34_Netzanschluss-50',
-'cl_2_quote_100-100-100_netz_40_pow_100-100-100_pause_45-540_M_35_Netzanschluss-40',
-'cl_2_quote_100-100-100_netz_30_pow_100-100-100_pause_45-540_M_36_Netzanschluss-30',
-'cl_2_quote_100-100-100_netz_20_pow_100-100-100_pause_45-540_M_37_Netzanschluss-20',
-'cl_2_quote_100-100-100_netz_10_pow_100-100-100_pause_45-540_M_38_Netzanschluss-10',
+# 'cl_2_quote_100-100-100_netz_90_pow_100-100-100_pause_45-540_M_30_Netzanschluss-90',
+# 'cl_2_quote_100-100-100_netz_80_pow_100-100-100_pause_45-540_M_31_Netzanschluss-80', 
+# 'cl_2_quote_100-100-100_netz_70_pow_100-100-100_pause_45-540_M_32_Netzanschluss-70', # Netzanschluss
+# 'cl_2_quote_100-100-100_netz_60_pow_100-100-100_pause_45-540_M_33_Netzanschluss-60', 
+# 'cl_2_quote_100-100-100_netz_50_pow_100-100-100_pause_45-540_M_34_Netzanschluss-50',
+# 'cl_2_quote_100-100-100_netz_40_pow_100-100-100_pause_45-540_M_35_Netzanschluss-40',
+# 'cl_2_quote_100-100-100_netz_30_pow_100-100-100_pause_45-540_M_36_Netzanschluss-30',
+# 'cl_2_quote_100-100-100_netz_20_pow_100-100-100_pause_45-540_M_37_Netzanschluss-20',
+# 'cl_2_quote_100-100-100_netz_10_pow_100-100-100_pause_45-540_M_38_Netzanschluss-10',
 ]
 
 if __name__ == '__main__':
